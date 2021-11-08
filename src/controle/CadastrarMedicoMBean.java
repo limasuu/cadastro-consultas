35c152e497dc3c021a62b64a0893daea5b0d3b9c
Suzane Lima <suzanelima@outlook.com>
2021-11-08 10:49:05 -0300

    Implementação do cadastro de médicos.

---
+++ src/controle/CadastrarMedicoMBean.java
@@ -0,0 +1,64 @@
+package controle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import modelo.Medico;
+
+@ManagedBean
+@SessionScoped
+public class CadastrarMedicoMBean {
+	
+	private Medico medico;
+	private List<Medico> medicos;
+	
+	public CadastrarMedicoMBean() {
+		
+		medico= new Medico();
+		medicos= new ArrayList<Medico>();
+	}
+	
+	public String entrarCadastro() {
+		
+		return "/cadastro-medico.jsf";
+	}
+	
+	public String voltar() {
+		
+		return "/index.jsf";
+	}
+	
+	public String cadastrar() {
+		
+		medicos.add(medico);
+		medico= new Medico();
+		
+		FacesMessage msg= new FacesMessage("Médico cadastrado com sucesso!");
+		msg.setSeverity(FacesMessage.SEVERITY_INFO);
+		
+		FacesContext.getCurrentInstance().addMessage("", msg);
+		
+		return "/cadastro-medico.jsf"; 
+	}
+
+	public Medico getMedico() {
+		return medico;
+	}
+
+	public void setMedico(Medico medico) {
+		this.medico = medico;
+	}
+
+	public List<Medico> getMedicos() {
+		return medicos;
+	}
+
+	public void setMedicos(List<Medico> medicos) {
+		this.medicos = medicos;
+	}	
+}
